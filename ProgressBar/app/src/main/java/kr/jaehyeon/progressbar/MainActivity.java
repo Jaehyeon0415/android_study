fb4293f6b534434c5010bbc11a55ce50e4b3dcf8
Jaehyeon <fourhf1@gmail.com>
2020-11-08 18:37:47 +0900

    ProgressBar

---
+++ ProgressBar/app/src/main/java/kr/jaehyeon/progressbar/MainActivity.java
@@ -0,0 +1,33 @@
+package kr.jaehyeon.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 뷰의 주소 값을 담을 참조 변수
+    ProgressBar bar4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bar4 = findViewById(R.id.progressBar4);
+    }
+
+    public void btnMethod(View view) {
+        bar4.incrementProgressBy(5);
+    }
+
+    public void btn2Method(View view) {
+        bar4.incrementProgressBy(-5);
+    }
+
+    public void btn3Method(View view) {
+        bar4.setProgress(80);
+    }
+}
